b0f463681e8ba53cc68c7f459a9f02da3e8ffc9d
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-03-05 22:10:02 -0300

    style/ start style for posts

--- CosmosSearch/src/components/Posts/PostsStyled.ts
+++ CosmosSearch/src/components/Posts/PostsStyled.ts
@@ -1,1 +1,17 @@
 import styled from "styled-components";
+
+export const PostListStyled = styled.ul`
+    width: 100%;
+    height: max-content;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`
+
+export const PostStyled = styled.li`
+    width: 100%;
+    height: 200px;
+
+    background-color: red;
+`
